833c49311742feb8422c890cdfbe9b436197ba52
isdance <isdance2004@hotmail.com>
2019-04-25 10:37:01 +1000

    update User Repository

--- repos/userRepo.js
+++ repos/userRepo.js
@@ -6,12 +6,35 @@ const findAll = async () => {
   return records
 }
 
+const findById = async id => {
+  const record = await User.findOne({ where: { id } })
+  return record
+}
+
 const create = async user => {
   const newUser = await User.create(user)
   return newUser
 }
 
+const update = async (id, user) => {
+  const userIdDb = await findById(id)
+
+  Object.keys(user).forEach(key => {
+    userIdDb[key] = user[key]
+  })
+  
+  userIdDb.save()
+}
+
+const del = async id => {
+  const record = await User.destroy({ where: { id } })
+  return record
+}
+
 module.exports = {
   findAll,
-  create
+  findById,
+  create,
+  update,
+  del
 }
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,33 @@
 var express = require('express')
 var router = express.Router()
-const { findAll, create } = require('../repos/userRepo')
+const { findAll, findById, create, update, del } = require('../repos/userRepo')
 
-/* GET users listing. */
 router.get('/', async (req, res) => {
   const users = await findAll()
   res.send(users)
 })
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  const user = await findById(id)
+  res.send(user)
+})
+
 router.post('/', async (req, res) => {
   const newUser = await create(req.body)
   res.status(201).send(newUser)
+})
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params
+  await update(id, req.body)
+  res.status(204).send()
+})
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+  await del(id)
+  res.status(204).send()
 })
 
 module.exports = router
